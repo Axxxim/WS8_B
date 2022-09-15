bbbaba1a593f959b18fd4933e8063f055c5cc02a
Axxxim <110543799+Axxxim@users.noreply.github.com>
2022-09-15 12:50:28 +1000

    some minore changes to the code

--- climate.py
+++ climate.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#seting the max coumns and width for initial
+pd.set_option('display.max_columns',None)
+pd.set_option('display.width',None)
 climate = pd.read_csv("climate.csv")
+
 years = climate['Year'].to_numpy()
 co2 = climate['CO2'].to_numpy()
 temp = climate['Temperature'].to_numpy()
 
 
 plt.subplot(2, 1, 1)
-plot(years, co2, 'b--') 
+plot(years, co2, temp) 
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
